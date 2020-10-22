f553be41482850ee92721cc1e10e53bfd07729bc
Vinicius Campos <viniciuscampos120@gmail.com>
2020-10-22 18:03:30 -0300

    feat(test): Start to implement the test cases

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+input_n = 3
+numbers = [(24, 1), (4358, 754), (305,794)]
+expected = [34, 1998, 1]
 
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def test(n, numbers, expected):
+    for i in range(n):
+        a,b = numbers[i]
+        assert solution(a,b) == expected[i]
 
+def solution(v1, v2):
+    return -1
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+def main(n, numbers, expected):
+    pass
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':    
+    test(input_n, numbers, expected)
